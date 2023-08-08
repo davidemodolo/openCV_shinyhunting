3a29d6a150e9f66ce49cb3d89a8d40c09fe5ee98
davidemodolo <davide.modolo98@gmail.com>
2023-08-08 13:12:07 +0200

    added for charmander and mudkip

--- autohunt.py
+++ autohunt.py
@@ -25,40 +25,52 @@ time.sleep(5)
 bot = telepot.Bot(values.TOKEN)
 
 
-def computer_vision():
+def computer_vision(reset_count):
     # import the image of the shiny pokemon
     shiny_png = np.array(cv2.imread(values.SHINY_PATH, cv2.IMREAD_UNCHANGED))
-    
+    # set from rgb to bgr
+    shiny_png = cv2.cvtColor(shiny_png, cv2.COLOR_BGRA2BGR)
     # take a screenshot of the main screen
     screen = ImageGrab.grab()
     screenshot = np.array(screen)
+
+    poochyena = np.array(cv2.imread("poochyena_sapphire.png", cv2.IMREAD_UNCHANGED))
+    poochyena = cv2.cvtColor(poochyena, cv2.COLOR_BGRA2BGR)
     # save the screenshot for future visual checks
     save_screenshot = np.array(screen)
     # convert images in order to check if shiny is presentp
-    shiny_png = cv2.cvtColor(shiny_png, cv2.COLOR_RGB2BGR)
+    # shiny_png = cv2.cvtColor(shiny_png, cv2.COLOR_RGB2BGR)
     screenshot = cv2.cvtColor(screenshot, cv2.COLOR_RGB2BGR)
+    # cv2.imshow('screenshot', screenshot)
+    # cv2.waitKey(0)
+    # cv2.imshow('shiny', shiny_png)
+    # cv2.waitKey(0)
+    # exit()
     # looking for the shiny
     result = cv2.matchTemplate(screenshot, shiny_png, cv2.TM_CCOEFF_NORMED)
+    result_poochyena = cv2.matchTemplate(screenshot, poochyena, cv2.TM_CCOEFF_NORMED)
+    min_val_p, max_val_p, min_loc_p, max_loc_p = cv2.minMaxLoc(result_poochyena)
 
     # max_val = max percentage of similarity
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
     print('matching ' + str(round(max_val*100, 2)) + '%')
+    print('matching poochyena ' + str(round(max_val_p*100, 2)) + '%')
     # format the screenshot in order to save it anyway
     new_p = Image.fromarray(save_screenshot)
-    if new_p.mode != 'RGB':
-        new_p = new_p.convert('RGB')
+    # if new_p.mode != 'RGB':
+    #     new_p = new_p.convert('RGB')
     # create the name of the screenshot
-    photoname = str(round(max_val*100, 2)) + "mudkip" + \
-        str(round(time.time()/1000, 2))[:-2]+".jpeg"
+    photoname = "mudkip" + f"{reset_count}" +".jpeg"
     # save the screenshot to the path
     path = values.SCREEN_PATH + photoname
-    new_p.save(path)
+    # new_p.save(path)
     # check if the shiny is found
     threshold = 0.99
-    if(max_val > threshold):
+    if(max_val > threshold and max_val_p > threshold):
         return True
     else:
         # if found shiny send photo with the pyhton bot
+        new_p.save(path)
         bot.sendPhoto(values.CHAT_ID, open(path, 'rb'))
         return False
 
@@ -66,7 +78,10 @@ def computer_vision():
 def redo():
     EMU_SPEED = 10  # emerald emulator speed
     # reset, start game, select the rightest pokeball (for mudkip) and start the battle
-    commands = "rnxxxxnxxxxxxxxxxnznxnxexxxn"
+    # commands = "rnxxxxnxxxxxxxxxxnznxnxexxxn" # FOR SQUIRTLE
+    # commands = "rnxxxxnxxxxxxxxxzznznxnxexxxn" # FOR CHARMANDER 266
+    commands = "rnxxxxxdxxnxn" #FOR MUDKIP
+
     for cr in commands:
         key = str(cr)
         if not (key == 'n'):
@@ -75,7 +90,7 @@ def redo():
             time.sleep(5/EMU_SPEED)
         # simulate each key pressed
         
-        
+        # print(key)
         keyboard.press(key)
         # a bit of delay between pressing and releasing to simulate better
         time.sleep(0.1)
@@ -91,7 +106,7 @@ def main():
     resets_count = int(file_resets.read())
     file_resets.close()
     # while no shiny found
-    while(computer_vision()):
+    while(computer_vision(resets_count)):
         # delay the next cycle
         time.sleep(2)
         redo()
--- values.py
+++ values.py
@@ -1,5 +1,5 @@
 TOKEN = '5016437106:AAHbigwvpexVn-shmOyzysmkeaekrYy_1aY'
-SHINY_PATH = 'sq.png'
+SHINY_PATH = 'mudkip_sapphire.png'
 SCREEN_PATH = "screens/"
 RESETS_PATH = "resets.txt"
 CHAT_ID = 113230043
